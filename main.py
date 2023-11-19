abdb961fff17d37e8f1d606a7d61b9f067f3ae7c
dalbongy <qhvien@gmail.com>
2023-11-19 17:58:34 +0100

    test added

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 
-import yaml
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv('OPEN_AI_KEY')
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=os.getenv('OPEN_AI_KEY'),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "What is your purpose?",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion)
+
